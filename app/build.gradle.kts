68231df194bdd76311f259681f77e053e64b2d68
heitormrosi <heitor2509@outlook.com>
2026-05-04 17:27:27 -0300

    update(agp-gradle): atualizando versões de diversos pacotes, incluindo o AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,12 @@
+import com.android.build.api.dsl.ApplicationExtension
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-parcelize")
 }
 
-android {
+configure<ApplicationExtension> {
     namespace = "dev.hmr.kanban"
     compileSdk = 36
 
@@ -32,12 +34,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
+
+
 }
 
 dependencies {
@@ -53,4 +55,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
